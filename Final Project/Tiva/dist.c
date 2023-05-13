cb52f4b80d6a147b696570cf6a7eef843c8d988d
Ahmed ibrahim <132453067+ahmeedibrahim@users.noreply.github.com>
2023-05-13 21:38:39 +0300

    Update dist.c

--- Final Project/Tiva/dist.c
+++ Final Project/Tiva/dist.c
@@ -13,3 +13,10 @@ float ToRad (double angle) {
 	return angle * PI / 180;
 }
 
+float GPS_getDistance(float currentLong, float currentLat, float destLong, float destLat) {
+float currentLongRad = ToRad(currentLong/100);
+float currentLatRad = ToRad(currentLat/100);
+float destLongRad = ToRad(destLong/100);
+float destLatRad = ToRad(destLat/100);
+}
+
