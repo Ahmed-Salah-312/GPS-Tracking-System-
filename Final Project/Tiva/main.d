e0741f5798c442c31506a02f5d66fd630414398d
Saied mohamed <133366488+Saa3ed@users.noreply.github.com>
2023-05-13 21:45:56 +0300

    Add files via upload

---
+++ Final Project/Tiva/main.d
@@ -0,0 +1,8 @@
+.\main.o: main.c
+.\main.o: gpio.h
+.\main.o: uart.h
+.\main.o: gps.h
+.\main.o: dist.h
+.\main.o: E:\ARM\ARMCC\bin\..\include\stdlib.h
+.\main.o: E:\ARM\ARMCC\bin\..\include\stdio.h
+.\main.o: E:\ARM\ARMCC\bin\..\include\math.h
